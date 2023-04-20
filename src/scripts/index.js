29d8e576a64cf753166e02ecb70e2a7858e399d5
Haerul Taka <haerultaka255@gmail.com>
2023-04-20 09:36:26 +0700

    mengubah index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,20 +8,20 @@ import("../DATA.json").then(({ default: jsonData }) => {
   let dataList = "";
   datas.forEach(function (data) {
     dataList += `
-        <div class="list_item">
-            <img class="list_item_thumb" src="${data["pictureId"]}" alt="${
+        <div class="card_item">
+            <img class="card_item_thumb" src="${data["pictureId"]}" alt="${
       data["name"]
     }" title="${data["name"]}">
             <div class="city">${data["city"]}</div>
-            <div class="list_item_content">
-                <p class="list_item_rating">
+            <div class="card_item_content">
+                <p class="card_item_rating">
                     Rating : 
-                    <a href="#" class="list_item_rating_value">${
+                    <a href="#" class="card_item_rating_value">${
                       data["rating"]
                     }</a>
                 </p>
-                <h1 class="list_item_title"><a href="#">${data["name"]}</a></h1>
-                <div class="list_item_desc">${data["description"].slice(
+                <h1 class="card_item_title"><a href="#">${data["name"]}</a></h1>
+                <div class="card_item_desc">${data["description"].slice(
                   0,
                   150
                 )}...</div>
